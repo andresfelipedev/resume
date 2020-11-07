d738ba768e03f1fe710fe085b63a47fc3649f120
andresfelipedev <andresfelipedev@gmail.com>
2020-11-07 14:19:57 -0500

    Updates

---
+++ precache-manifest.e34fca60abe8b2a9136164df915e408a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e2d4911a4e0b78a7fde3f83f1610134",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "d73819af6dcc702e162f",
+    "url": "/resume/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "299ba401232ff3db7111",
+    "url": "/resume/static/css/main.e9416cd5.chunk.css"
+  },
+  {
+    "revision": "d73819af6dcc702e162f",
+    "url": "/resume/static/js/2.acc71485.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/resume/static/js/2.acc71485.chunk.js.LICENSE"
+  },
+  {
+    "revision": "299ba401232ff3db7111",
+    "url": "/resume/static/js/main.22d93a21.chunk.js"
+  },
+  {
+    "revision": "e3be495ebf93da27ea4e",
+    "url": "/resume/static/js/runtime-main.687946a0.js"
+  },
+  {
+    "revision": "1b7f89296a423bb549fef9cc9f5dfdaa",
+    "url": "/resume/static/media/profile-picture.1b7f8929.jpg"
+  }
+]);
